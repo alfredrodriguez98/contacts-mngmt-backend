1efa4983c4740c1f5e9c2c1ea32c8454b35ff1ae
alfredrodriguez98 <alfredrodriguez1998@outlook.com>
2023-04-08 19:15:24 +0530

    Server setup with response on port specified in env

---
+++ server.js
@@ -0,0 +1,24 @@
+//Creating an express server
+const express = require("express");
+//To use dynamic port value from .env file
+const dotenv = require("dotenv").config();
+
+//Create an express app
+const app = express();
+
+//Define a port
+// const port = 3000; - hardcoding a static server port. But can be done
+// dynamically as shown below
+const port = process.env.PORT || 5000;
+
+app.get("/api/contacts", (req, res) => {
+  res.send("Get all contacts");
+});
+
+//Listen to server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+//To test the APIs, we need http client - using thunderclient extension to avoid
+//switching between vs code and postman
